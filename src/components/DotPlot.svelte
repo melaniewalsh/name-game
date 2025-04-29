59dc2c70c4c30ef66209a7fe6523a005cd40eb56
Michelle McGhee <michmac202@gmail.com>
2025-04-29 11:36:33 -0700

    svg dot plot

--- src/components/DotPlot.svelte
+++ src/components/DotPlot.svelte
@@ -10,24 +10,88 @@
 
 	let data = dataOptions[id];
 
-	let width = $state(0);
-	const xScale = $derived(scaleLinear().domain([0, 100]).range([0, width]));
-	const xGet = (d) => +d.proportion_female * 100;
+	const margin = {
+		top: 40,
+		right: 45,
+		bottom: 10,
+		left: 80
+	};
 
-	$inspect({ data, width });
+	let svgWidth = $state(0);
+	let svgHeight = $state(0);
+	let chartWidth = $derived(svgWidth - margin.right - margin.left);
+	let chartHeight = $derived(svgHeight - margin.top - margin.bottom);
+
+	const xScale = $derived(
+		scaleLinear().domain([0, 100]).range([0, chartWidth])
+	);
+	const xGet = (d) => +d.proportion_female * 100;
+	const xAxisLabels = [
+		"100% he/him",
+		"75% he/him",
+		"Equal",
+		"75% she/her",
+		"100% she/her"
+	];
+	const yScale = $derived(
+		scaleLinear().domain([0, data.length]).range([0, chartHeight])
+	);
 </script>
 
 <figure id={`dot-plot-${id}`}>
 	<h3>{title}</h3>
-	<div>{sub}</div>
+	<h4>{sub}</h4>
 
-	<div class="chart-container" bind:clientWidth={width}>
-		{#each data as d}
-			<div class="row">
-				<!-- <div class="label">{d.animal_group}</div> -->
-				<div class="emoji" style:left={`${xScale(xGet(d))}px`}>{d.emoji}</div>
-			</div>
-		{/each}
+	<div
+		class="chart-container"
+		bind:clientWidth={svgWidth}
+		bind:clientHeight={svgHeight}
+	>
+		<svg>
+			<g transform={`translate(${margin.left}, ${margin.top})`}>
+				<g class="y-axis">
+					{#each data as d, i}
+						<line
+							x1="-15"
+							y1={yScale(i)}
+							x2={chartWidth + 15}
+							y2={yScale(i)}
+							stroke="var(--color-gray-200)"
+						/>
+						<text x="-20" y={yScale(i)}>{d.animal_group}</text>
+					{/each}
+				</g>
+
+				<g class="x-axis">
+					{#each [0, 25, 50, 75, 100] as d, i}
+						<line
+							x1={xScale(d)}
+							y1="-15"
+							x2={xScale(d)}
+							y2={chartHeight - 15}
+							stroke="var(--color-gray-200)"
+							class:equal={d === 50}
+						/>
+						<text x={xScale(d)} y={chartHeight}>{xAxisLabels[i]}</text>
+					{/each}
+
+					<text x={xScale(50) - 15} y={-30} style:text-anchor={"end"}
+						>{"<-"} more he/him</text
+					>
+					<text x={xScale(50) + 15} y={-30} style:text-anchor={"start"}
+						>more she/her {"->"}</text
+					>
+				</g>
+
+				<g class="animals">
+					{#each data as d, i}
+						<text x={xScale(xGet(d))} y={yScale(i)}>
+							{d.emoji}
+						</text>
+					{/each}
+				</g>
+			</g>
+		</svg>
 	</div>
 </figure>
 
@@ -36,18 +100,44 @@
 		display: flex;
 		flex-direction: column;
 		gap: 2rem;
+		height: 400px;
 	}
 
-	.row {
-		position: relative;
-		display: flex;
-		align-items: center;
-		height: 2px;
-		background: var(--color-gray-200);
+	h3 {
+		font-size: var(--24px);
+		text-align: center;
 	}
 
-	.emoji {
-		position: absolute;
-		transform: translate(-50%, 0);
+	h4 {
+		font-size: var(--14px);
+		text-align: center;
+	}
+
+	svg {
+		width: 100%;
+		height: 100%;
+	}
+
+	line.equal {
+		stroke: var(--color-gray-800);
+		stroke-width: 2px;
+		stroke-dasharray: 10;
+	}
+
+	.x-axis text {
+		font-size: var(--12px);
+		text-anchor: middle;
+	}
+
+	.y-axis text {
+		font-size: var(--14px);
+		text-anchor: end;
+		dominant-baseline: middle;
+	}
+
+	.animals text {
+		font-size: var(--24px);
+		text-anchor: middle;
+		dominant-baseline: middle;
 	}
 </style>
